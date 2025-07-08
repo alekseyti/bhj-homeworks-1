3fa7b49e98e46dd2c3d9a187ba35f20c0cc31ed2
alekseyti <68515628+alekseyti@users.noreply.github.com>
2025-07-08 22:23:03 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,33 +1,3 @@
-// Повышение уровня сложности задачи: реализовать обратный отсчет времени в формате hh:mm:ss, а не просто в секундах.
-// const timer = document.getElementById('timer');
-
-// // Перевод времени в секунды
-// function timeToSeconds(timeStr) {
-//   const [hours, minutes, seconds] = timeStr.split(':').map(Number);
-//   return hours * 3600 + minutes * 60 + seconds;
-// }
-
-// // Форматирование секунд в hh:mm:ss
-// function formatTime(seconds) {
-//   const hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
-//   const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
-//   const secs = String(seconds % 60).padStart(2, '0');
-//   return `${hrs}:${mins}:${secs}`;
-// }
-
-// let timeLeft = timeToSeconds(timer.textContent);
-
-// const intervalId = setInterval(() => {
-//   if (timeLeft <= 0) {
-//     clearInterval(intervalId);
-//     alert('Вы победили в конкурсе!');
-//   } else {
-//     timeLeft--;
-//     timer.textContent = formatTime(timeLeft);
-//   }
-// }, 1000);
-
-
 const timer = document.getElementById('timer');
 
 let timeLeft = Number(timer.textContent);
